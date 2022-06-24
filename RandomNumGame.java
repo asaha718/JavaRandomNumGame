ae357d8d776e38dad0f9a7dae58b23d2cc22c71b
Anug Saha <anugsaha@gmail.com>
2022-06-24 10:29:46 -0400

    working version of easy

--- RandomNumGame.java
+++ RandomNumGame.java
@@ -1,10 +1,21 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomNumGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Anug's Random Number Game!");
         System.out.println("Please Select a number from 1-10 :");
-        // System.out.println(generateRandomNumber());
+        try (Scanner scanner = new Scanner(System.in);) {
+            int num = 0;
+            String numInput = scanner.nextLine();
+            num = Integer.parseInt(numInput);
+
+            if(num > generateRandomNumber()){ 
+                System.out.println("You win!");
+            }else{ 
+                System.out.println("System wins! Better luck next time"); 
+            }
+        }
 
     }
 
