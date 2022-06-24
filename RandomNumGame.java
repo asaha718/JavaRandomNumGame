235c6ac69a6ad83c3ff38d2f1cbe086452ebf5e4
Anug Saha <anugsaha@gmail.com>
2022-06-24 10:38:05 -0400

    level logic

--- RandomNumGame.java
+++ RandomNumGame.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class RandomNumGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Anug's Random Number Game!");
+
+    }
+
+    public static int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(11);
+    }
+
+    public static void easyVersion() {
         System.out.println("Please Select a number from 1-10 :");
+
         try (Scanner scanner = new Scanner(System.in);) {
             int num = 0;
             String numInput = scanner.nextLine();
@@ -16,11 +26,39 @@ public class RandomNumGame {
                 System.out.println("System wins! Better luck next time"); 
             }
         }
-
     }
 
-    public static int generateRandomNumber() {
-        Random random = new Random();
-        return random.nextInt(11);
+    public static void mediumVersion() {
+        System.out.println("Please Select a number from 1-10 :");
+
+        try (Scanner scanner = new Scanner(System.in);) {
+            int num = 0;
+            String numInput = scanner.nextLine();
+            num = Integer.parseInt(numInput);
+
+            if(num > generateRandomNumber()){ 
+                System.out.println("Your number was higher so you WIN!");
+            }
+            
+            if(num <= generateRandomNumber()){ 
+                System.out.println("Your number was lower so you LOSE! System has won"); 
+            }
+        }
+    }
+    
+    public static void hardVersion() {
+        System.out.println("Please Select a number from 1-10 :");
+
+        try (Scanner scanner = new Scanner(System.in);) {
+            int num = 0;
+            String numInput = scanner.nextLine();
+            num = Integer.parseInt(numInput);
+
+            if(num == generateRandomNumber()){ 
+                System.out.println("You win!");
+            }else{ 
+                System.out.println("System wins! Better luck next time"); 
+            }
+        }
     }
 }
