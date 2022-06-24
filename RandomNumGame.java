9f5ac24a293598febf1690986bd5f64614217875
Anug Saha <anugsaha@gmail.com>
2022-06-24 10:56:26 -0400

    working version of game

--- RandomNumGame.java
+++ RandomNumGame.java
@@ -4,7 +4,33 @@ import java.util.Scanner;
 public class RandomNumGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Anug's Random Number Game!");
+        String[] explanationMessage = {
+                "Easy : guess a number - program will tell you if it was higher or equal (you win) or lower (computer wins) than the program's number",
+                "Meduim : guess a number - program will tell you if it was strictly higher (you win) or lower or equal (computer wins) than the program's number",
+                "Hard : guess a number - program will tell you if it was equal (you win) or not (you lose) to the program's number"
+        };
 
+        System.out.println("Would you like to play Easy(e), Medium(m), Hard(h) : "); 
+
+        try (Scanner scanner = new Scanner(System.in);) {
+            String levelInput = scanner.nextLine();
+
+            if( levelInput.equals("e") ){ 
+                System.out.println(explanationMessage[0]); 
+                easyVersion();
+            }
+
+            if( levelInput.equals("m")){ 
+                System.out.println(explanationMessage[1]); 
+                mediumVersion();
+            }
+
+            if( levelInput.equals("h")){ 
+                System.out.println(explanationMessage[2]); 
+                hardVersion();
+            }
+
+        } 
     }
 
     public static int generateRandomNumber() {
@@ -20,10 +46,10 @@ public class RandomNumGame {
             String numInput = scanner.nextLine();
             num = Integer.parseInt(numInput);
 
-            if(num > generateRandomNumber()){ 
+            if (num > generateRandomNumber()) {
                 System.out.println("You win!");
-            }else{ 
-                System.out.println("System wins! Better luck next time"); 
+            } else {
+                System.out.println("System wins! Better luck next time");
             }
         }
     }
@@ -36,16 +62,16 @@ public class RandomNumGame {
             String numInput = scanner.nextLine();
             num = Integer.parseInt(numInput);
 
-            if(num > generateRandomNumber()){ 
+            if (num > generateRandomNumber()) {
                 System.out.println("Your number was higher so you WIN!");
             }
-            
-            if(num <= generateRandomNumber()){ 
-                System.out.println("Your number was lower so you LOSE! System has won"); 
+
+            if (num <= generateRandomNumber()) {
+                System.out.println("Your number was lower so you LOSE! System has won");
             }
         }
     }
-    
+
     public static void hardVersion() {
         System.out.println("Please Select a number from 1-10 :");
 
@@ -54,10 +80,10 @@ public class RandomNumGame {
             String numInput = scanner.nextLine();
             num = Integer.parseInt(numInput);
 
-            if(num == generateRandomNumber()){ 
+            if (num == generateRandomNumber()) {
                 System.out.println("You win!");
-            }else{ 
-                System.out.println("System wins! Better luck next time"); 
+            } else {
+                System.out.println("System wins! Better luck next time");
             }
         }
     }
