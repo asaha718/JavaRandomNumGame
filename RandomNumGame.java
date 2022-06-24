5249aee8c03c208fd77061e95558be607bf8ab39
Anug Saha <anugsaha@gmail.com>
2022-06-24 10:15:50 -0400

    generate random number

--- RandomNumGame.java
+++ RandomNumGame.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class RandomNumGame {
     public static void main(String[] args) {
-        System.out.println("Running...");
+        System.out.println("Welcome to Anug's Random Number Game!");
+        System.out.println("Please Select a number from 1-10 :");
+        // System.out.println(generateRandomNumber());
+
     }
 
-    public void generateRandomNumber() {
+    public static int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(11);
     }
 }
